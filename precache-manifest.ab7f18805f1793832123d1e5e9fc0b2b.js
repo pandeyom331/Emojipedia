ac6901d6d38f7ede145985597998a42495375f28
pandeyom331 <pandeyom331@github.com>
2021-06-04 20:39:39 +0530

    Updates

---
+++ precache-manifest.ab7f18805f1793832123d1e5e9fc0b2b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21ab227ede7295e8d5832c02c182acd",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "56a82c49317e22b93801",
+    "url": "/Emojipedia/static/js/2.c9890295.chunk.js"
+  },
+  {
+    "revision": "49e0168c8b5a3189c3a9",
+    "url": "/Emojipedia/static/js/main.241f9364.chunk.js"
+  },
+  {
+    "revision": "6224630e21eed1f82ef3",
+    "url": "/Emojipedia/static/js/runtime-main.52cd7e16.js"
+  }
+]);
